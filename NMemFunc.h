f0be74646b00f31785ab311617f5a5078a8fa0c8
luliqwerty <151127972+luliqwerty@users.noreply.github.com>
2024-01-20 22:44:06 +0800

    Create NMemFunc.h

---
+++ NMemFunc.h
@@ -0,0 +1,12 @@
+#ifndef NMEMFUNC_H_INCLUDED
+#define NMEMFUNC_H_INCLUDED
+#include<string>
+using namespace std;
+//计算manager个数
+int CalculateManagerFileLen();
+
+//计算goods个数
+int CalculateGoodsFileLen();
+
+int  CalculateGoodsInfoFileLen();
+#endif // NMEMFUNC_H_INCLUDED
